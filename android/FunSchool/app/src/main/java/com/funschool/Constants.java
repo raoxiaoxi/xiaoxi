65064db75618cbe971ca0fd65356773e4c8d7459
yybo <yybo@yybodeMacBook-Pro.local>
2016-07-01 13:00:01 +0800

    更新

---
+++ android/FunSchool/app/src/main/java/com/funschool/Constants.java
@@ -0,0 +1,8 @@
+package com.funschool;
+
+/**
+ * 公共常量
+ * Created by yybo on 16/7/1.
+ */
+public class Constants {
+}
--- android/FunSchool/app/src/main/java/com/funschool/main/HomeFragment.java
+++ android/FunSchool/app/src/main/java/com/funschool/main/HomeFragment.java
@@ -1,13 +1,23 @@
 package com.funschool.main;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.funschool.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by yybo on 16/6/28.
@@ -15,6 +25,21 @@ import android.widget.TextView;
 public class HomeFragment extends Fragment implements MainContract.View{
 
     private MainContract.Presenter mPresenter;
+
+    private Activity mActivity;
+    private View layout;
+    private GridView gridView;
+    private SimpleAdapter saItem;
+    private String[] gridviewNames;
+
+    private static int[] gridview_imgs = {R.mipmap.card_icon,
+            R.mipmap.card_icon,
+            R.mipmap.card_icon,
+            R.mipmap.card_icon,
+            R.mipmap.card_icon,
+            R.mipmap.card_icon};
+
+    ArrayList<HashMap<String, Object>> meumList = new ArrayList<HashMap<String, Object>>();
 
     public static HomeFragment newInstance() {
         Bundle arguments = new Bundle();
@@ -27,9 +52,55 @@ public class HomeFragment extends Fragment implements MainContract.View{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        TextView mTextView = new TextView(getActivity());
-        mTextView.setText("首页");
-        return mTextView;
+        if (layout == null) {
+            mActivity = this.getActivity();
+            layout = mActivity.getLayoutInflater().inflate(R.layout.fragment_main,
+                    null);
+            initViews();
+            setValues();
+
+            saItem = new SimpleAdapter(mActivity,
+                    meumList,
+                    R.layout.gridview_item,
+                    new String[]{"ItemImage","ItemText"},
+                    new int[]{R.id.item_image,R.id.item_text});
+
+            Log.i("适配器数据",saItem.toString());
+
+            Log.i("gridView",gridView.toString());
+            //添加Item到网格中
+            gridView.setAdapter(saItem);
+            //添加点击事件
+            gridView.setOnItemClickListener(
+                    new AdapterView.OnItemClickListener() {
+                        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                            Toast.makeText(mActivity, gridviewNames[arg2], Toast.LENGTH_SHORT).show();
+                            //Toast用于向用户显示一些帮助/提示
+                        }
+                    }
+            );
+//            setOnListener();
+        } else {
+            ViewGroup parent = (ViewGroup) layout.getParent();
+            if (parent != null) {
+                parent.removeView(layout);
+            }
+        }
+        return layout;
+    }
+
+    private void initViews() {
+        gridView = (GridView) layout.findViewById(R.id.gridView);
+    }
+
+    private void setValues() {
+        gridviewNames = mActivity.getResources().getStringArray(R.array.gridview_list);
+        for (int i=0; i<gridviewNames.length; i++) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("ItemImage", gridview_imgs[i]);
+            map.put("ItemText", gridviewNames[i]);
+            meumList.add(map);
+        }
     }
 
     @Override
